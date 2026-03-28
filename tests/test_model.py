5969a125598b384f038e9d09567a620c0cfb10df
Vamsi Reddy <vamsireddykeesara123@gmail.com>
2026-03-28 11:43:17 +0530

    finaltesting

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,4 +2,4 @@ def test_accuracy_threshold():
     with open("model/artifacts/accuracy.txt", "r") as f:
         accuracy = float(f.read())
 
-    assert accuracy >= 0.90, f"❌ Accuracy too low: {accuracy}"
+    assert accuracy >= 0.94, f"❌ Accuracy too low: {accuracy}"             
